957680b27dc3baad3b38c972d2de23968f450be1
Bororda <vasilevichviktor@gmail.com>
2023-03-28 19:42:09 +0200

    initial

---
+++ Reference project/Rampage Navigator Open Source Files v1.7.3/README.md
@@ -0,0 +1,43 @@
+WELCOME TO THE RALLY RAMPAGE NAVIGATION CONTROLLER
+
+This project is for developing a bluetooth handlebar controller to use for navigation whilst riding motorbike.
+
+The project was started by Eugene Beetge from Rally Rampage. The original designs and coding are all from Eugene.
+
+For more information please visit: 
+
+https://www.rallyrampage.com
+
+or 
+
+email at info@rallyrampage.com
+
+REVISIONS & RELEASES
+
+Version 1.7.3 Release date 2022/03/31
+-added default mode option to startup in an app
+-added battery testing and display on mobile using keyboard text when in Mode select and pushing button 5 which is unassigned with an app. Used now for battery voltage testing and display push
+-cleaned up code in other areas
+-better notes inside code
+
+Version 1.7.2  (Release date 2021/12/17)
+- updated code for all 4 new apps
+	Locus Maps
+	Rally Navigator
+	OsMand Maps
+	Piste Roadbook
+
+Version 1.7  (Release date 2021/12/14)
+- complete new functionality using rocker switches.
+- rocker one mode is to select one of 5 apps, with LED flicker to confirm app selected.
+- rocker two mode is using the app selected.
+- accomodating for normaly-open and normaly-closed controller buttons in code. Easy to select
+- code half size, double the speed
+
+Version 1.5  (Release date 2021/09/27)
+- included 3d print files
+- 2 versions of 3d print files with different designs to make manufacturing easier
+- permanent power option in the design
+
+Version 1.2 (Release date 2021/06/27)
+- original open source files
---
+++ controller/rampage_navigator/rampage_navigator.ino
@@ -0,0 +1,89 @@
+#include <BleKeyboard.h>
+
+#define led 32
+
+//PUSH BUTTONS
+#define button1 25
+#define button2 23
+#define button3 33
+
+//TOGGLE SWITCH BUTTONS
+#define button4 18
+#define button5 19
+
+BleKeyboard bleKeyboard;
+
+unsigned long debounce1;
+unsigned long debounce2;
+int lag = 300;
+
+bool buttonOnePressed = false;
+bool buttonTwoPressed = false;
+bool buttonThreePressed = false;
+
+void setup() {
+  bleKeyboard.begin();
+  delay(5000);
+  
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+  pinMode(button5, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+
+  //Attachinterrupt
+  setAttaches();
+}
+
+void loop() {
+  if (millis() - debounce1 > lag) {
+    if (digitalRead(button4) == LOW) {
+      bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+    }
+    if (digitalRead(button5) == LOW) {
+      bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
+    }
+    debounce1 = millis();
+  }
+  if (buttonOnePressed) {
+    bleKeyboard.print("c");
+    buttonOnePressed = false;
+  }
+  if (buttonTwoPressed) {
+    bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+    buttonTwoPressed = false;
+  }
+  if (buttonThreePressed) {
+    bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+    buttonThreePressed = false;
+  }
+}
+/*
+When we flag a piece of code with the IRAM_ATTR attribute, 
+the compiled code is placed in the ESP32’s Internal RAM (IRAM). 
+Otherwise the code is kept in Flash. And Flash on ESP32 is much slower than internal RAM.
+*/
+void IRAM_ATTR buttonOne() {
+  if (millis() - debounce2 > lag) {
+    buttonOnePressed = true;
+    debounce2 = millis();
+  }
+}
+void IRAM_ATTR buttonTwo() {
+  if (millis() - debounce2 > lag) {
+    buttonTwoPressed = true;
+    debounce2 = millis();
+  }
+}
+void IRAM_ATTR buttonThree() {
+  if (millis() - debounce2 > lag) {
+    buttonThreePressed = true;
+    debounce2 = millis();
+  }
+}
+void setAttaches() {  //has to be after the functions, otherwise fails with not declared with the IRAM_ATTR
+  attachInterrupt(digitalPinToInterrupt(button1), buttonOne, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button2), buttonTwo, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button3), buttonThree, FALLING);
+}
